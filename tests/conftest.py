0a56c4fbf1f427de8829c8fa6bd95e880b85d8a2
VoronkoV_Y <voronkov-y@yandex.ru>
2026-05-26 04:18:01 -0700

    All tasks done

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import pytest
-from src.my_classes import Product, Category
+from src.my_classes import Product, Category, Smartphone, LawnGrass
 
 
 @pytest.fixture
 def product_fixture():
     return Product("Test_name", "Test description", 54.99, 65)
 
+
 @pytest.fixture
 def product_2_fixture():
     return Product("Testo", "Testo's description", 100, 33)
+
 
 @pytest.fixture
 def category_1_fixture():
@@ -34,11 +36,73 @@ def category_2_fixture():
 
 @pytest.fixture
 def user_product_dict():
-    return {"name": "Samsa", "description": "Very tasty samsa", "price": 180,
-         "quantity": 5}
+    return {
+        "name": "Samsa",
+        "description": "Very tasty samsa",
+        "price": 180,
+        "quantity": 5,
+    }
 
 
 @pytest.fixture
 def user_product_dict2():
-    return {"name": "Samsa", "description": "Very tasty samsa", "price": 135,
-         "quantity": 95}
+    return {
+        "name": "Samsa",
+        "description": "Very tasty samsa",
+        "price": 135,
+        "quantity": 95,
+    }
+
+
+@pytest.fixture
+def smartphone1_fixture():
+    return Smartphone(
+        "Xiaomi",
+        "description norm tel",
+        100,
+        5,
+        "efficiency-Xiaomi",
+        "note 9",
+        "128GB",
+        "White",
+    )
+
+
+@pytest.fixture
+def smartphone2_fixture():
+    return Smartphone(
+        "Sony",
+        "description norm Sonyl",
+        200,
+        12,
+        "efficiency-Sony",
+        "Super model",
+        "64GB",
+        "Grey",
+    )
+
+
+@pytest.fixture
+def lawngrass1_fixture():
+    return LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый",
+    )
+
+
+@pytest.fixture
+def lawngrass2_fixture():
+    return LawnGrass(
+        "Газонная super трава 2",
+        "Выносливая трава",
+        450.0,
+        15,
+        "США",
+        "5 дней",
+        "Темно-зеленый",
+    )
